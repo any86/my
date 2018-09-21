beb777533b4ce8945f3be0f289d72e22bd028c9f
宁宁 <383514580@qq.com>
2018-09-21 15:59:25 +0800

    增加倒计时功能, 待开发pause和resume功能

---
+++ src/CountDown.ts
@@ -0,0 +1,111 @@
+interface TimeData {
+    day?: string;
+    hour?: string;
+    minute?: string;
+    second: string;
+}
+// 倒计时
+class CountDown {
+    private _timeoutId: number;
+    private callbacks: { [propName: string]: ((data?: any) => void)[] };
+
+    // 倒计时秒数
+    public countDownNumber: number;
+    public precision: number;
+
+    constructor(countDownNumber: number, precision: number = 3) {
+        this.countDownNumber = countDownNumber;
+        this.precision = precision;
+    }
+
+    public start() {
+        // 倒计时
+        if (0 < this.countDownNumber) {
+            this._timeoutId = setTimeout(() => {
+                this.countDownNumber--;
+                let timeData: TimeData = this._parseCountDownNumber(this.countDownNumber, this.precision);
+
+                if (undefined !== this.callbacks.change) {
+                    this.callbacks.change.forEach(callback => {
+                        callback(timeData);
+                    });
+                }
+            }, 1000);
+        } else {
+            clearTimeout(this._timeoutId);
+            this.callbacks.end.forEach(callback => {
+                callback();
+            });
+        }
+        return this;
+    };
+
+    public on(eventName: string, callback: (data: TimeData) => void) {
+        this.callbacks[eventName].push(callback);
+        return this;
+    };
+
+    public pause() {
+        return this;
+    };
+
+    /**
+     * 解析数字成'秒分时日'格式
+     * @param {Number} 秒数
+     * @param {Number} 精度, 1:秒, 2:分, 3:时, 4:日.
+     */
+    private _parseCountDownNumber = (number: number, precision: number = 1): any => {
+        switch (precision) {
+            case 1: {
+                return { second: number };
+            };
+
+            case 2: {
+                let second: string = String(number % 60).padStart(2, '0');
+                let minute = String(Math.floor(number / 60)).padStart(2, '0');
+                return {
+                    minute,
+                    second
+                };
+            };
+
+            case 3: {
+                let second = String(number % 60).padStart(2, '0');
+                let minute = String(Math.floor(number / 60) % 60).padStart(2, '0');
+                let hour = String(Math.floor(number / 3600)).padStart(2, '0');
+                return {
+                    hour,
+                    minute,
+                    second
+                };
+            };
+
+            case 4: {
+                let second = String(number % 60).padStart(2, '0');
+                let minute = String(Math.floor(number / 60) % 60).padStart(2, '0');
+                let hour = String(Math.floor(number / 3600) % 24).padStart(2, '0');
+                let day = String(Math.floor(number / 86400)).padStart(2, '0');
+                return {
+                    day,
+                    hour,
+                    minute,
+                    second
+                };
+            };
+
+            default: {
+                return {
+                    second: number
+                };
+            };
+        }
+    }
+}
+
+
+// let cd = new CountDown(1000,3).start();
+// cd.pause();
+// cd.resume()
+
+
+
