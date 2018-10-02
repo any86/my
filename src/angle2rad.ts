f09402beb8789a22e8356fbb46d1445fa2121bf7
PC-Ning <383514580@qq.com>
2018-10-02 20:58:27 +0800

    新增angle2rad: 角度转弧度

---
+++ src/angle2rad.ts
@@ -0,0 +1,7 @@
+/**
+ * 角度转弧度
+ * @param {Number} 角度
+ * @param {Boolean} 是否对结果进行四舍五入取整
+ * @return {Number} 弧度
+ */
+export default (angle: number, isRound:boolean = false): number => angle * Math.PI / 180;
