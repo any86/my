e75dc299746b3138384863a1e2accc73845fde6d
宁宁 <383514580@qq.com>
2018-10-24 16:43:31 +0800

    更改一些变量名

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,23 +1,22 @@
+// 待绑定事件目标
+type Target = Element | Document | Window;
 // 事件的回调
-interface EventHandler {
+interface EventListener {
     (payload: object): void;
 };
 
 // 事件堆栈
-interface Stack {
-    [propsName: string]: EventHandler[];
+interface EventsStore {
+    [propsName: string]: EventListener[];
 };
 
-// 待绑定事件目标
-type Target = Element | Document | Window;
-
 export default class EventEmitter {
-    private _stack: Stack;
+    private _eventStore: EventsStore;
     public target: Target;
     public hasDomEvent: boolean;
 
     constructor(target: Target = document, { hasDomEvent = false } = {}) {
-        this._stack = <Stack>{};
+        this._eventStore = <EventsStore>{};
         this.target = target;
         this.hasDomEvent = hasDomEvent;
     };
@@ -27,16 +26,16 @@ export default class EventEmitter {
      * @param {String} 事件名
      * @param {Object} 参数
      */
-    emit(eventName: string, payload: { [propName: string]: any }): void {
-        if (undefined !== this._stack[eventName]) {
-            this._stack[eventName].forEach(handler => {
-                handler(payload);
+    emit(type: string, payload: { [propName: string]: any }): void {
+        if (undefined !== this._eventStore[type]) {
+            this._eventStore[type].forEach(listener => {
+                listener(payload);
             });
         }
 
         if (this.hasDomEvent) {
             // 创建浏览器事件
-            const event: any = new CustomEvent(eventName, {
+            const event: any = new CustomEvent(type, {
                 detail: payload,
                 bubbles: true,
                 cancelable: true
@@ -57,11 +56,11 @@ export default class EventEmitter {
      * @param {String} 事件名
      * @param {Function} 回调函数
      */
-    on(eventName: string, handler: EventHandler) {
-        if (undefined === this._stack[eventName]) {
-            this._stack[eventName] = [] as EventHandler[];
+    on(type: string, listener: EventListener) {
+        if (undefined === this._eventStore[type]) {
+            this._eventStore[type] = [] as EventListener[];
         };
-        this._stack[eventName].push(handler);
+        this._eventStore[type].push(listener);
     };
 
     /**
@@ -69,14 +68,14 @@ export default class EventEmitter {
      * @param {String} 事件名 
      * @param {Function} 事件回调
      */
-    off(eventName: string, handler: EventHandler) {
-        let handlers = this._stack[eventName];
-        if (undefined === handler) {
+    off(type: string, listener: EventListener) {
+        let handlers = this._eventStore[type];
+        if (undefined === listener) {
             handlers = [];
         } else {
             for (let i = 0, len = handlers.length; i < len; i++) {
                 let existHandler = handlers[i];
-                if (existHandler === handler) {
+                if (existHandler === listener) {
                     handlers.splice(i, 1);
                     break;
                 }
@@ -89,15 +88,15 @@ export default class EventEmitter {
      * @param {String} 事件名
      * @return {Boolean} 是否存在
      */
-    has(eventName: string): boolean {
-        return undefined !== this._stack[eventName];
+    has(type: string): boolean {
+        return undefined !== this._eventStore[type];
     };
 
     /**
      * 销毁注册事件
      */
     destory() {
-        this._stack = null;
+        this._eventStore = null;
         this.target = null;
     };
 };
