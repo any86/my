b3e4899f66766429ff1d136d3e295e2cdc9ebdb0
any86 <383514580@qq.com>
2022-05-04 16:15:37 +0800

    perf: repeat执行结果队列到数组中

--- packages/repeat/src/index.ts
+++ packages/repeat/src/index.ts
@@ -1,6 +1,8 @@
 export default function (times: number, callbcak: (i: number) => void, start: number = 0) {
+    const array = [];
     const l = times + start
     for (let j = start; j < l; j++) {
-        callbcak(j);
+        array.push(callbcak(j));
     }
+    return array;
 } 
